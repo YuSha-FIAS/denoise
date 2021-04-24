5e2c21b0d87c313295e48800dd6688894479e5ff
luhavefun <58715138+luhavefun@users.noreply.github.com>
2021-04-24 19:21:58 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,2 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# @FileName          :utils.py
-# @Time              :2021/4/19 13:09
-# @Author            :Yu Sha
-# @Mail              :sy_shayu@163.com
-# @PRODUCT_NAME}     :PyCharm2019.1.3
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import warnings
 
-warnings.filterwarnings("ignore")
  
